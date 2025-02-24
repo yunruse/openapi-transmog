a4a08cf5807f0234f83431a3ab25435d93d997b1
Mia yun Ruse <mia@yunru.se>
2025-02-24 11:08:46 +0000

    Handle call annotators far, far better
    ast_syntax_error is adorable. It got me curious a little as to how Python does it internally; turns out ast.AST.end_offset was in 3.8, long before it was used in 3.10 for nice messages.
    
    Hope this is helpful, anyway, and not a total headache. You can never tell with AST stuff, can you?

--- openapi_transmog/annotations.py
+++ openapi_transmog/annotations.py
@@ -3,8 +3,9 @@ from copy import deepcopy
 from dataclasses import dataclass
 from pathlib import Path
 from sys import stderr
+from typing import Generator
 
-from .helpers import split_by_predicate
+from .helpers import ast_syntax_error, argument_count, split_by_predicate
 
 
 def fdef_has_content(fdef: FunctionDef):
@@ -23,17 +24,11 @@ def fdef_has_content(fdef: FunctionDef):
 @dataclass
 class Argument:
     fdef: FunctionDef
-
-    def _first_arg(self):
-        return self.fdef.args.args[0]
-
-    @property
-    def name(self):
-        return self._first_arg().arg
+    name: str
 
     @property
     def type(self):
-        return self._first_arg().annotation
+        return self.fdef.args.args[0].annotation
 
     @property
     def default(self):
@@ -85,6 +80,127 @@ class Applicator(NodeTransformer):
         return node
 
 
+CallMapping = tuple[str, str]
+
+
+def get_call_annotators(
+    fdef: FunctionDef, dcrtr: Call, fp: Path, src: str
+) -> Generator[CallMapping, None, None]:
+    """
+    For a single decorator, list its function arg name -> API arg name mapping.
+
+    For example, if a user used
+
+    @+MyFunc(my_var='myVar', foo_bar='fooBar')
+    def my_annotator(xyz: list[str]) -> str:
+        return ','.join(xyz)
+
+    they are saying that the generated MyFunc(my_var, foo_bar)
+    will pass in those arguments as {'myVar': my_var, 'fooBar': foo_bar}
+    when calling the API endpoint.
+
+    Alternatively, as (potentially confusing?) shorthand, they may use
+
+    @+MyFunc('myVar')
+    def my_annotator(xyz: list[str]) -> str:
+        return ','.join(xyz)
+
+    in which the function's variable name is used, eg {'myVar': xyz}.
+
+    This handles all the above and (hopefully?) passes sane SyntaxErrors.
+    """
+
+    if not isinstance(dcrtr.func, Name):
+        raise ast_syntax_error(
+            dcrtr.func,
+            "Function annotators need a function name to annotate",
+            fp, src)
+
+    assert isinstance(dcrtr.func, Name), "Decorator should be the name of a function!"
+
+    N = argument_count(fdef.args)
+
+    if N == 0:
+        raise ast_syntax_error(
+            fdef,
+            "Argument annotators cannot work on functions that take no input",
+            # TODO: why not? We could delete the parameter,
+            # hiding it from the encapsulation?
+            fp, src)
+
+    if N > 1 or fdef.args.kwarg or fdef.args.vararg:
+        raise ast_syntax_error(
+            fdef,
+            f"Argument annotators can only modify one parameter."
+            " For more complex transformations, consider a wrapper function"
+            f" to encapsulate {dcrtr.func.id}.",
+            fp, src)
+
+    assert N == 1 and len(fdef.args.args) == 1, "incomprehensible function definition"
+    default_dest = fdef.args.args[0].arg
+
+    if len(dcrtr.args) == len(dcrtr.keywords) == 0:
+        args = [f'{a.value}={a.value!r}' for a in dcrtr.args]
+        more_explicit = f"@+{dcrtr.func.id}({default_dest!r})"
+        raise ast_syntax_error(
+            dcrtr,
+            "Argument annotators requires at least one argument:"
+            f" the key of the parameter to pass to {dcrtr.func.id}."
+            "\n Check the OpenAPI schema or run without annotations"
+            " to find which that might be."
+            f"\nConsider: {more_explicit}",
+            fp, src)
+
+    def is_string_literal(node):
+        return isinstance(node, Constant) and isinstance(node.value, str)
+    if not all(map(is_string_literal, dcrtr.args)):
+        raise ast_syntax_error(
+            dcrtr,
+            "Argument annotators should take only strings,"
+            " representing the API parameter key",
+            fp, src)
+
+    if len(dcrtr.args) > 1:
+        args = [f'{a.value}={a.value!r}' for a in dcrtr.args]
+        more_explicit = f"@+{dcrtr.func.id}({', '.join(args)})"
+
+        raise ast_syntax_error(
+            dcrtr.args[1],
+            "Cannot annotate an API call with multiple argument names in this way:"
+            f" only one can be renamed {default_dest}."
+            f"\nConsider: {more_explicit}",
+            fp, src)
+
+    if len(dcrtr.args) and len(dcrtr.keywords):
+        args = [f'{a.value}={a.value!r}' for a in dcrtr.args]
+        args += [f'{k.arg}={unparse(k.value)}' for k in dcrtr.keywords]
+        more_explicit = f"@+{dcrtr.func.id}({', '.join(args)})"
+
+        A0 = repr(dcrtr.args[0].value)
+        mapping = '{%s: %s}' % (A0, default_dest)
+
+        raise ast_syntax_error(
+            dcrtr,
+            "Argument annotators can only take either one name --"
+            f" turning the API call into something like"
+            f"\n    def {dcrtr.func.id}({A0}): get(params={mapping}) --"
+            "\nor multiple arguments, which would provide explicit"
+            "function argument names to embody each respective API parameter key."
+            f"\nConsider: {more_explicit}",
+            fp, src
+        )
+
+    # and that's syntaxerrors! wow, writing AST stuff is a pain.
+
+    # yield func_arg_name, api_arg_name
+
+    for arg in dcrtr.args:
+        yield default_dest, arg.value
+
+    for arg in dcrtr.keywords:
+        yield arg.arg, arg.value.value
+
+
 def get_annotations(fp: Path):
     """
     For a file path to a Python file, parse it, return its AST nodes, and also info on the argument and property 'annotators'.
@@ -106,7 +222,8 @@ def get_annotations(fp: Path):
     @+ is used to ensure any other decorators are ignored.
     """
 
-    body = parse(fp.read_text()).body
+    src = fp.read_text()
+    body = parse(src).body
 
     # {func_name: {arg_name: arg}}
     arguments: dict[str, dict[str, Argument]] = {}
@@ -127,19 +244,14 @@ def get_annotations(fp: Path):
 
             if isinstance(dcrtr, Call):
                 # @value(arg_name, ...)
-                # -> applies to the argument to an API call; we want the annotator's argument type
-                assert len(fdef.args.args) == 1, "Function must take only one parameter!"
-
-                assert isinstance(dcrtr.func, Name)
-                for arg in dcrtr.args:
-                    assert isinstance(arg, Constant)
-
-                    argdef = Argument(fdef)
-                    arguments.setdefault(dcrtr.func.id, {})[arg.value] = argdef
+                # -> applies to the argument to an API call
+                for func_arg, api_arg in get_call_annotators(fdef, dcrtr, fp, src):
+                    argdef = Argument(fdef, func_arg)
+                    arguments.setdefault(dcrtr.func.id, {})[api_arg] = argdef
 
             elif isinstance(dcrtr, Subscript):
                 # @value['prop_name', ...]
-                # -> applies to the property of a TypedDict; we want the annotator's return type
+                # -> applies to the property of a TypedDict
 
                 continue
                 # TODO: apply _from_api:
--- openapi_transmog/helpers.py
+++ openapi_transmog/helpers.py
@@ -4,6 +4,8 @@ Project-agnostic helper functions.
 
 
 from typing import Callable, TypeVar
+import ast
+import pathlib
 
 T = TypeVar('T')
 
@@ -31,3 +33,54 @@ def walk_dict(d: dict):
         if isinstance(v, (list, tuple)):
             for v2 in v:
                 yield from walk_dict(v)
+
+
+def ast_syntax_error(node: ast.AST, message: str, file_path: pathlib.Path = None, _src: str = None) -> SyntaxError:
+    """
+    Raise a syntax error from an AST node.
+
+    file_path may be None, though the result will miss ^^^ annotations.
+
+    By default, _src = file_path.read_text(), but it can be provided if file_path is stdin or a socket or something.
+    """
+
+    file_path.is_fifo()
+    err = SyntaxError(message)
+
+    if not file_path:
+        # rather unlikely a SyntaxError would be raised on a developer's own homebrewed AST,
+        # so lack of ^^^^ annotations is not really an issue!
+        err.filename = "<unknown>"
+        err.text = ast.unparse(ast.fix_missing_locations(node))
+        err.lineno = 1
+        return err
+
+    err.filename = file_path
+    if _src is None:
+        _src = pathlib.Path(file_path).read_text()
+
+    if hasattr(node, 'lineno'):
+        err.lineno = node.lineno
+        err.text = _src.splitlines()[err.lineno - 1]
+
+        if hasattr(node, 'end_lineno'):
+            err.end_lineno = node.end_lineno
+            # TODO: multiline err.text..?
+
+    if hasattr(node, 'col_offset'):
+        # TODO: check if +1 is just an error on my system or something
+        err.offset = node.col_offset + 1
+        if hasattr(node, 'end_col_offset'):
+            err.end_offset = node.end_col_offset + 1
+
+    return err
+
+
+def argument_count(args: ast.arguments):
+    "Number of arguments into a function."
+    N = len(args.args + args.kwonlyargs + args.posonlyargs)
+    if args.kwarg:
+        N += 1
+    if args.vararg:
+        N += 1
+    return N
